ee9ee39bd6edd9f754d8f215d552ad6a740debe0
shuhei <shuyume0522@gmail.com>
2019-03-23 00:58:09 +0900

    Laravel10 kadai

---
+++ AAAController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AAAController extends Controller
+{
+public function bbb(){
+  return view('admin.XXXXXX')
+}
+
+}
